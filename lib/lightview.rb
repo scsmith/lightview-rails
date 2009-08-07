bec07b6c69fcd9f449f3d6ff185633fd7c3f37ab
Steve Smith <stevesmith@host217-37-15-193.in-addr.btopenworld.com>
2009-08-07 16:58:49 +0100

    added link_to_close_modal to create a link that can close a modal window

--- lib/lightview.rb
+++ lib/lightview.rb
@@ -11,6 +11,23 @@ module Lightview
       options[:method] ||= :get
       link_to_remote(name, options, html_options)
     end
+    
+    def link_to_close_modal(*args, &proc)
+      if block_given?
+        options      = args.first || {}
+        html_options = args.second
+        concat(link_to_close_modal(capture(&block), options, html_options))
+      else
+        name         = args.first
+        options      = args.second || {}
+        html_options = args.third || {}
+      end
+
+      html_options[:onclick] = (html_options[:onclick] || "") + "Lightview.hide(); return false;"
+      html_options[:href] ||= '#'
+
+      link_to(name, options, html_options)
+    end
   end
   
   module PrototypeHelper
@@ -22,15 +39,15 @@ module Lightview
     
     def remote_function_with_modal(options)
       return remote_function_without_modal(options) unless options[:modal] == true
-  
+      
       javascript_options = options_for_ajax(options)
-  
+      
       # Cannot do update form for lightview windows
       raise "Sorry update is not supported for modal windows" if options[:update]
-  
+      
       url_options = options[:url]
       url_options = url_options.merge(:escape => false) if url_options.is_a?(Hash)
-  
+      
       function =
         "Lightview.show({
           href: '#{escape_javascript(url_for(url_options))}',
@@ -40,12 +57,12 @@ module Lightview
             ajax: #{javascript_options}
           }
         })"
-  
+      
       function = "#{options[:before]}; #{function}" if options[:before]
       function = "#{function}; #{options[:after]}"  if options[:after]
       function = "if (#{options[:condition]}) { #{function}; }" if options[:condition]
       function = "if (confirm('#{escape_javascript(options[:confirm])}')) { #{function}; }" if options[:confirm]
-  
+      
       return function
     end
   end
